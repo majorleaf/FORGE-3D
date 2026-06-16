0da544c1748c9b460b0a4cbfda25daccf7990de2
Madandola james <jamesmadandola@gmail.com>
2026-06-16 04:50:09 +0100

    fixed config

--- config/supabaseClient.js
+++ config/supabaseClient.js
@@ -1,8 +1,10 @@
+import 'dotenv/config';
+
 import { createClient } from "@supabase/supabase-js";
 
 const rawUrl = process.env.SUPABASE_URL || "";
 const supabaseUrl = rawUrl.replace(/\/rest\/v1\/?$/, "");
-const supabaseKey = process.env.SUPABASE_KEY || process.env.SUPABASE_ANON_KEY || process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY || process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_URL;
 
 
 const supabase = createClient(supabaseUrl, supabaseKey, {
