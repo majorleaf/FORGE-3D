e649a93a95e6a2e7b65b7bf176adccbad03495bb
Madandola james <jamesmadandola@gmail.com>
2026-09-17 11:04:31 +0100

    rendering, error, authtokenization route, layout appendition

---
+++ src/app/api/auth/token/route.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+import { createClient } from "@/lib/supabase/server";
+
+export async function GET()  {
+    const supabase = await createClient()
+    const { data: {session } } = await supabase.auth.getSession()
+    return NextResponse.json({ token: session?.access_token})
+}
