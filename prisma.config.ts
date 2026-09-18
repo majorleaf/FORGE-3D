fad80c756d006d33c4486677cfaa9653964c153f
Madandola james <jamesmadandola@gmail.com>
2026-09-18 23:09:44 +0100

    prisma wahala

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,4 @@
+import 'dotenv/config'
 import { defineConfig } from 'prisma/config'
 
-export default defineConfig({
-  datasource: {
-    url: process.env.DATABASE_URL!,
-  },
-})
+export default defineConfig({})
